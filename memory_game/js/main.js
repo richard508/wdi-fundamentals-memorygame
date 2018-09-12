e390c718d43257979da6131b9b8fda463990d7f9
Richard Almonte <richard.almonte@infosurv.com>
2018-09-12 18:12:28 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,19 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
 
+var cardsInPlay = [];
 
-console.log("User flipped " + cardOne);
+var cardOne;
+cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardsInPlay);
+var cardTwo;
+cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+  alert("You found a match!")
+}
+else {
+  alert("Sorry, try again!")
+};
